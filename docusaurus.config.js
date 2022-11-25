a70e532e4e2d831e4110da61992a44b88dd2ef6a
moloewe-leuphana <moloewe@leuphana.de>
2022-11-25 17:13:07 +0100

    change a bit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,10 +6,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: ' MoLoewe - Mobile Lösungswerkstatt',
+  title: 'MoLoewe - Mobile Lösungswerkstatt',
   tagline: 'Faculty of Sustainability - Leuphana University',
   url: 'https://moloewe-leuphana.github.io',
-  baseUrl: '/docs/',
+  baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'moloewe-leuphana', // Usually your GitHub org/user name.
   projectName: 'moloewe-leuphana.github.io', // Usually your repo name.
-  deploymentBranch: 'main',      // Branch for deployment
+  deploymentBranch: 'deployment',      // Branch for deployment
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
