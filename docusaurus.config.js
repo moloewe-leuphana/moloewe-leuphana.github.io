b35072f2415b51bc33920a1ab3699184f239a628
Stephen Kurniawan <stepkurniawan@gmail.com>
2022-11-28 17:04:26 +0100

    test autodeploy 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'MoLöwe - Mobile Lösungswerkstatt',
-  tagline: 'Faculty of Sustainability - Leuphana University',
+  tagline: 'Faculty of Sustainability -- Leuphana University',
   url: 'https://moloewe-leuphana.github.io',
   baseUrl: '/',
   onBrokenLinks: 'warn',
