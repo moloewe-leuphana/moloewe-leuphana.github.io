cca4d848f40f72482f91432160ed594522568471
moloewe-leuphana <moloewe@leuphana.de>
2022-12-09 12:16:33 +0100

    landing page edits e.g. dbu logo, contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,35 +4,24 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import PrintFAQ from '@site/src/components/FAQList';
-
-
 // import styles from './index.module.css';
 import styles from '@site/src/pages/index.module.css'
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBannerContact)}>
-        <div className="container">
-         <h1>{'Leuphana University Lüneburg'}</h1>
-         <p>{'Faculty of Sustainability'}</p>
-         <p>{'Universitätsallee 1, 21335 Lüneburg, Germany'}</p>
-         <p>{'moloewe@leuphana.de'}</p>
-        </div>
-    </header>
-
-  );
-}
 
 // Where the structure of the page lives
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Mobile Lösungswerkstatt (MoLöwe)`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
+    <Layout title={`Mobile Lösungswerkstatt (MoLöwe)`}>
+    <div className={styles.background}>
+        <div className={styles.table}>
+            <div className={styles.cell}>
+                <h1>{'Leuphana University Lüneburg'}</h1>
+                <p>{'Faculty of Sustainability'}</p>
+                <p>{'Universitätsallee 1, 21335 Lüneburg, Germany'}</p>
+                <p>{'moloewe@leuphana.de'}</p>
+            </div>
+        </div>
+    </div>
 
     </Layout>
   );
--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -4,7 +4,8 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import PrintFAQ from '@site/src/components/FAQList';
-import styles from './index.module.css';
+// import styles from './index.module.css';
+import styles from '@site/src/pages/index.module.css'
 
 import img1 from '/img/gallery/01.JPG';
 import img2 from '/img/gallery/02.JPG';
@@ -14,18 +15,6 @@ import img5 from '/img/gallery/05.JPG';
 import img6 from '/img/gallery/06.JPG';
 import img7 from '/img/gallery/07.JPG';
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={'header'}>
-      <div className={clsx('container', styles.heroBanner)}>
-        <h1 className="hero__title">{'Mobile Solution Workshop'}</h1>
-        <h1 className="hero__title__2">{'Gallery'}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
 
 // Where the structure of the page lives
 export default function Home() {
@@ -35,94 +24,9 @@ export default function Home() {
       title={`Mobile Lösungswerkstatt (MoLöwe)`}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
 
-      <main>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50',
-            fontSize: '20px',
-            padding: '5% 10%',
-          }}
-        >
-          <img src={img1}></img>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50',
-            fontSize: '20px',
-            padding: '5% 10%',
-          }}
-        >
-          <img src={img2}></img>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50',
-            fontSize: '20px',
-            padding: '5% 10%',
-          }}
-        >
-          <img src={img3}></img>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50',
-            fontSize: '20px',
-            padding: '5% 10%',
-          }}
-        >
-          <img src={img4}></img>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50',
-            fontSize: '20px',
-            padding: '5% 10%',
-          }}
-        >
-          <img src={img5}></img>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50',
-            fontSize: '20px',
-            padding: '5% 10%',
-          }}
-        >
-          <img src={img6}></img>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50',
-            fontSize: '20px',
-            padding: '5% 10%',
-          }}
-        >
-          <img src={img7}></img>
-        </div>
-      </main>
+    <div className={styles.background}>
+    </div>
     </Layout>
   );
 }
