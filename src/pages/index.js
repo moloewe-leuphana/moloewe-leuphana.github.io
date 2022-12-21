66235c8eb6a1559d73562c5f0da90b696aabe170
moloewe-leuphana <moloewe@leuphana.de>
2022-12-21 10:03:50 +0100

    landing page background

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,36 +10,17 @@ import styles from './index.module.css';
 var abstractText =
   'The Mobile Solution Workshop is a new approach for participative workshops developed at the Faculty of Sustainability. This digital environment allows to visualise future scenarios and to experience the effects of adjusted model parameters in real time. It consists of the hardware, including five 65’’ screens that create an immersive environment and tablets that allow participants to interact with the screens’ content, as well as the custom developed software for this interactive dashboard. The entire set-up is mobile and can therefore be put into action on site. For this reason alone, scientist can reach a larger target audience with the Mobile Solution Workshop. Complex problems are fanned out and visualised in comprehensible steps across the five connected 65’’ screens. Participants can experience and design interactive scenarios. To this end, the Mobile Solution Workshop allows to visualise effects of interventions in real-time; illustrates the interplay between local actions and global drivers and to make abstract facts tangible.';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{'Mobile Lösungswerkstatt (MoLöwe)'}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/welcome-moloewe">
-            Go to Manual
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 // Where the structure of the page lives
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={`Mobile Lösungswerkstatt (MoLöwe)`}>
 
-    <div className={styles.background}>
-    </div>
+    <div className={styles.background}></div>
 
     <div className={styles.bg_text}>
-      <div>MoLöwe (Mobile Lösungswerkstatt)</div>
+        <h1 className="hero__title">{'Mobile Lösungswerkstatt (MoLöwe)'}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
     </div>
 
     <div className={clsx('hero hero--primary', styles.abstractBanner)}>
