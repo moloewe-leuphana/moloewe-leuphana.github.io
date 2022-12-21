5265684c6686e5a28125c1668745f4b4c86a7298
moloewe-leuphana <moloewe@leuphana.de>
2022-12-21 11:22:39 +0100

    landing page background

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,11 +16,12 @@ export default function Home() {
   return (
     <Layout title={`Mobile Lösungswerkstatt (MoLöwe)`}>
 
-    <div className={styles.background}></div>
-
-    <div className={styles.bg_text}>
-        <h1 className="hero__title">{'Mobile Lösungswerkstatt (MoLöwe)'}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <div>
+        <div className={styles.background}></div>
+            <div className={styles.bg_text}>
+                <h1 className="hero__title">{'Mobile Lösungswerkstatt (MoLöwe)'}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+            </div>
     </div>
 
     <div className={clsx('hero hero--primary', styles.abstractBanner)}>
